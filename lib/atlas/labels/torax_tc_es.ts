42af6c279328aee1f278e6b7bc0c94939ad8119d
Victor <vcpolanco@gmail.com>
2026-01-21 00:12:41 -0300

    Add atlas helpers and thorax TC Spanish labels

---
+++ lib/atlas/labels/torax_tc_es.ts
@@ -0,0 +1,31 @@
+// lib/atlas/labels/torax_tc_es.ts
+export const TORAX_TC_LABELS_ES: Record<string, string> = {
+  
+  trachea: 'Tráquea',
+  carina: 'Carina',
+  right_main_bronchus: 'Bronquio principal derecho',
+  left_main_bronchus: 'Bronquio principal izquierdo',
+
+  right_lung: 'Pulmón derecho',
+  left_lung: 'Pulmón izquierdo',
+
+  ascending_aorta: 'Aorta ascendente',
+  aortic_arch: 'Arco aórtico',
+  descending_aorta: 'Aorta descendente',
+  pulmonary_trunk: 'Tronco pulmonar',
+  right_pulmonary_artery: 'Arteria pulmonar derecha',
+  left_pulmonary_artery: 'Arteria pulmonar izquierda',
+  superior_vena_cava: 'Vena cava superior',
+  inferior_vena_cava: 'Vena cava inferior',
+
+  right_atrium: 'Aurícula derecha',
+  left_atrium: 'Aurícula izquierda',
+  right_ventricle: 'Ventrículo derecho',
+  left_ventricle: 'Ventrículo izquierdo',
+
+  esophagus: 'Esófago',
+
+  sternum: 'Esternón',
+  thoracic_vertebra: 'Vértebra torácica',
+  rib: 'Costilla',
+}
